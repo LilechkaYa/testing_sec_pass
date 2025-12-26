302acc83d62e587350c8f238d87a0c0c513a601c
Lilita Nikuskina <lilita.nikuskina@team.blue>
2025-12-26 09:38:14 +0000

    html updated to a fancier one!

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
+
 from flask import Flask, render_template, request
 import io
-import sys
 from contextlib import redirect_stdout
-from sec_pass.tester import make_whmcs_request, set_server_id # We will tweak tester.py slightly
+from sec_pass.tester import make_whmcs_request, set_server_id
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    output = ""
+    output = None
+    server_id = None
     if request.method == 'POST':
         server_id = request.form.get('server_id')
         if server_id:
-            # Capture the print statements from your script
+            # We still capture stdout for now so you don't have to 
+            # rewrite all of tester.py, but we clean it up for the UI
             f = io.StringIO()
             with redirect_stdout(f):
-                # Pass the ID to your existing logic
                 set_server_id(server_id)
                 make_whmcs_request()
             output = f.getvalue()
     
-    return render_template('index.html', output=output)
+    return render_template('index.html', output=output, server_id=server_id)
 
 if __name__ == "__main__":
+    # Internal port 8000 is correct for Gunicorn/Docker
     app.run(host='0.0.0.0', port=8000)
